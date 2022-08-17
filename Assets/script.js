36ea8923a15ba4d0cd411d6d08dc04232ea91fe0
AKam <andy.kaminski42@gmail.com>
2022-08-17 15:38:32 -0400

    Created a check on what time it was, added event to object in timeBlocksArray

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,46 @@
+$(document).ready(function() {
+
+})
 // DOM Elements
 var timeDisplayEl = $('#currentDay');
 var timeBlocksEl = $('#timeBlocks');
 
 // Time Blocks Variables
 var timeBlocksArray = [
-    {display: "9AM", time:9},
-    {display: "10AM", time:10},
-    {display: "11AM", time:11},
-    {display: "12AM", time:12},
-    {display: "1PM", time:13},
-    {display: "2PM", time:14},
-    {display: "3PM", time:15},
-    {display: "4PM", time:16},
-    {display: "5PM", time:17}
+    {display: "9AM", time:9, event:""},
+    {display: "10AM", time:10, event:""},
+    {display: "11AM", time:11, event:""},
+    {display: "12AM", time:12, event:""},
+    {display: "1PM", time:13, event:""},
+    {display: "2PM", time:14, event:""},
+    {display: "3PM", time:15, event:""},
+    {display: "4PM", time:16, event:""},
+    {display: "5PM", time:17, event:""}
 ];
-var events = ["","","","","","","","",""];
 
 // Display Date Function
 function displayDate() {
-    var today = moment().format('dddd, MMMM Do YYYY');
-    timeDisplayEl.text(today);
+    timeDisplayEl.text(moment().format('dddd, MMMM Do YYYY'));
 }
 
 // Display Time Blocks Function
+function displayTimeBlocks() {
+  for (var i=0; i<timeBlocksArray.length; i++) {
+    var hourNow = parseInt(moment().format("HH"));
+    var blockHour = timeBlocksArray[i].time;
+    var timeClass = "";
+
+    // Assigns class based on what time it is
+    if(hourNow > blockHour) {
+        timeClass = "past";
+    } else if (hourNow === blockHour) {
+        timeClass = "present";
+    } else {
+        timeClass = "future";
+    };
+
+  }
+}       
 
 // Function to Store Events
 
